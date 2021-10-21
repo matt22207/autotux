4ea03c6e6c5dd4c0f538656c049edefb4318c1de
matt22207 <matt22207@gmail.com>
2021-10-21 18:14:15 -0400

    initial commit

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade
+sudo apt full-upgrade
+
+sudo apt install gnome-tweaks neofetch barrier
+
+# https://flatpak.org/setup/Ubuntu/
+sudo apt install flatpak 
+sudo apt install gnome-software-plugin-flatpak
+      
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+flatpak install flathub org.gnome.Extensions
+
+## PAUSE HERE TO REBOOT
