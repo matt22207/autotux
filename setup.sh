418097204506fc94c9e55bb620bea49c70835f27
matt22207 <matt22207@gmail.com>
2021-11-20 15:56:53 -0500

    force apt

--- setup.sh
+++ setup.sh
@@ -6,28 +6,27 @@
 gsettings set org.gnome.desktop.interface gtk-theme Adwaita-dark
 gsettings set org.gnome.desktop.interface gtk-theme Yaru-dark
 
-sudo apt update
-sudo apt upgrade
+sudo apt update -y && sudo apt upgrade -y
 #sudo apt full-upgrade
 
-sudo apt install gnome-tweaks neofetch git openssh-server net-tools htop timeshift
+sudo apt install -y gnome-tweaks neofetch git openssh-server net-tools htop timeshift
 
 # KVM thin provisioning tools, virt-sparsify - https://www.certdepot.net/kvm-thin-provisioning-tip/
-sudo apt install libguestfs-tools
+sudo apt install -y libguestfs-tools
 
 # https://cockpit-project.org/running.html#ubuntu
-sudo apt install cockpit cockpit-machines cockpit-pcp
+sudo apt install -y cockpit cockpit-machines cockpit-pcp
 
 # https://flatpak.org/setup/Ubuntu/
-sudo apt install flatpak 
-sudo apt install gnome-software-plugin-flatpak
+sudo apt install -y flatpak 
+sudo apt install -y gnome-software-plugin-flatpak
       
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 sudo flatpak install flathub org.gnome.Extensions
 
 # Setup libvirt for Single GPU Passhthrough - https://gitlab.com/risingprismtv/single-gpu-passthrough/-/wikis/4)-Configuring-of-Libvirt
-sudo apt install qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virt-manager ovmf
+sudo apt install -y qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virt-manager ovmf
 
 # latest barrier is in snap. doesn't support Wayland yet
 sudo snap install barrier
