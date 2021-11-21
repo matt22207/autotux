ea14ffe69694445ef82a04c611f69f381372130c
matt22207 <matt22207@gmail.com>
2021-11-20 19:38:40 -0500

    APT_PACKAGES

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@
 BACKUP_PATH=~/.setup_backups
 GRUB_CFG_PATH=/etc/default/grub
 VFIO_CFG_PATH=/etc/dracut.conf.d/vfio.conf
+APT_PACKAGES=""
 
 # create a directory for any backups
 mkdir ${BACKUP_PATH}
@@ -16,25 +17,28 @@ gsettings set org.gnome.desktop.interface gtk-theme Yaru-dark
 sudo apt update -y && sudo apt upgrade -y
 #sudo apt full-upgrade
 
-sudo apt install -y gnome-tweaks neofetch git openssh-server net-tools htop timeshift
+APT_PACKAGES+="gnome-tweaks neofetch git openssh-server net-tools htop timeshift flatpak "
 
 # KVM thin provisioning tools, virt-sparsify - https://www.certdepot.net/kvm-thin-provisioning-tip/
-sudo apt install -y libguestfs-tools
+APT_PACKAGES+="libguestfs-tools "
 
 # https://cockpit-project.org/running.html#ubuntu
-sudo apt install -y cockpit cockpit-machines cockpit-pcp
+APT_PACKAGES+="cockpit cockpit-machines cockpit-pcp "
 
 # https://flatpak.org/setup/Ubuntu/
-sudo apt install -y flatpak 
-sudo apt install -y gnome-software-plugin-flatpak
-      
-sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
-sudo flatpak install flathub org.gnome.Extensions
+APT_PACKAGES+="flatpak gnome-software-plugin-flatpak "
 
 # Setup libvirt for Single GPU Passhthrough - https://gitlab.com/risingprismtv/single-gpu-passthrough/-/wikis/4)-Configuring-of-Libvirt
-sudo apt install -y qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virt-manager ovmf
+APT_PACKAGES+="qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virt-manager ovmf "
 
+sudo apt install -y $APT_PACKAGES
+
+
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo flatpak update
+sudo flatpak install flathub org.gnome.Extensions
+
+sudo snap refresh
 # latest barrier is in snap. doesn't support Wayland yet
 sudo snap install barrier
 
