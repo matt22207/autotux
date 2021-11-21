3c41f3c4ea98ceb1e98ce638500e0476998ffdaf
matt22207 <matt22207@gmail.com>
2021-11-20 19:21:34 -0500

    grub updates

--- setup.sh
+++ setup.sh
@@ -2,6 +2,13 @@
 
 # wget -O - https://raw.githubusercontent.com/matt22207/autotux/main/setup.sh | bash
 
+BACKUP_PATH=~/.setup_backups
+GRUB_CFG_PATH=/etc/default/grub
+VFIO_CFG_PATH=/etc/dracut.conf.d/vfio.conf
+
+# create a directory for any backups
+mkdir ${BACKUP_PATH}
+
 # https://itsfoss.com/fedora-dark-mode/
 gsettings set org.gnome.desktop.interface gtk-theme Adwaita-dark
 gsettings set org.gnome.desktop.interface gtk-theme Yaru-dark
@@ -35,3 +42,37 @@ sudo snap install barrier
 
 # TODO : Grub font size
 # https://vietlq.github.io/2019/09/22/make-grub-font-size-bigger/
+
+
+# modify grub via: https://github.com/T-vK/MobilePassThrough/blob/master/utils/Ubuntu/21.04/kernel-param-utils
+
+GRUB_UPDATE_REQUIRED=0
+cp ${GRUB_CFG_PATH} "${BACKUP_PATH}/grub_$(date +%Y%m%d_%H%M%S)"
+
+function addKernelParam() {
+    if ! sudo cat "$GRUB_CFG_PATH" | grep "GRUB_CMDLINE_LINUX=" | grep --quiet "$1"; then
+        sudo sed -i "s/^GRUB_CMDLINE_LINUX=\"/&$1 /" "$GRUB_CFG_PATH"
+        echo "addKernelParam: Added \"$1\" to GRUB_CMDLINE_LINUX in $GRUB_CFG_PATH"
+        GRUB_UPDATE_REQUIRED=1
+    else
+        echo "addKernelParam: No action required. \"$1\" already exists in GRUB_CMDLINE_LINUX of $GRUB_CFG_PATH"
+    fi
+}
+
+echo "Adding kernel parameters..."
+# Docs: https://www.kernel.org/doc/Documentation/admin-guide/kernel-parameters.txt
+# More docs: https://lwn.net/Articles/252826/
+# https://www.kernel.org/doc/Documentation/x86/x86_64/boot-options.txt
+echo "Adding kernel parameters to enable IOMMU on Intel/AMD CPUs..."
+
+# https://gitlab.com/risingprismtv/single-gpu-passthrough/-/wikis/2)-Editing-GRUB
+addKernelParam "iommu=pt"
+addKernelParam "intel_iommu=on" # enable Intel VT-D   ;# using "intel_iommu=on,igfx_off" iGPU gets no iommu group...
+addKernelParam "apparmor=1"
+addKernelParam "security=apparmor"
+addKernelParam "udev.log_priority=3"
+
+if GRUB_UPDATE_REQUIRED == 1; then
+    echo "Applying the kernel parameter changes..."
+    sudo update-grub
+fi
