ec2f4ae3a6ef059dff073ecd3dc95b8448512f91
matt22207 <matt22207@gmail.com>
2021-12-23 14:24:55 -0500

    add iostat

--- setup.sh
+++ setup.sh
@@ -19,7 +19,7 @@ gsettings set org.gnome.desktop.interface gtk-theme Yaru-dark
 sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y
 #sudo apt full-upgrade
 
-APT_PACKAGES+="gnome-tweaks neofetch git openssh-server net-tools htop timeshift flatpak firefox chrome-gnome-shell python3-pip screen "
+APT_PACKAGES+="gnome-tweaks neofetch git openssh-server net-tools htop timeshift flatpak firefox chrome-gnome-shell python3-pip screen systat"
 APT_PACKAGES+="nvidia-driver-470 nvidia-utils-470 nvidia-settings "
 # TODO: remove xserver-xorg-video-nouveau
 
