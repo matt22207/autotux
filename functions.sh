e26d7f355f59a88b4fd016e1c2e0f0971306988a
matt22207 <matt22207@gmail.com>
2022-01-25 12:26:43 -0500

    sudo var in func

--- functions.sh
+++ functions.sh
@@ -2,8 +2,8 @@
 
 function appendLineToFile() {
     echo "appending : [ $1 ] to $2"
-    if ! sudo cat "$2" | grep "$1"; then
-        echo "$1" | sudo tee -a $2
+    if ! ${SUDO} cat "$2" | grep "$1"; then
+        echo "$1" | ${SUDO} tee -a $2
     else 
         echo "-- No changes needed"
     fi
@@ -11,8 +11,8 @@ function appendLineToFile() {
 
 function replaceLineInFile() {
     echo "replacing : [ $1 ] with [ $2 ] in $3"
-    if sudo cat "$3" | grep "$1"; then    
-        sudo sed -i "s/^$1/$2/" "$3"
+    if ${SUDO} cat "$3" | grep "$1"; then    
+        ${SUDO} sed -i "s/^$1/$2/" "$3"
     else 
         echo "-- No changes needed"
     fi
