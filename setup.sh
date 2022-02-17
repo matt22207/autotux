a3e8dc8f8c077ecb11341627f570dee1ce2c3c42
matt22207 <matt22207@gmail.com>
2022-02-17 18:02:34 -0500

    add ExtensionManager

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@
 # cd autotux
 # git pull; bash ./setup.sh
 #
+# TODO add execution bit to setup.sh
 
 BACKUP_PATH=~/.setup_backups
 GRUB_CFG_PATH=/etc/default/grub
@@ -68,7 +69,7 @@ ${PACKAGE_MANAGER_BIN} ${PACKAGE_MANAGER_UPDATE_CMD}
 
 PACKAGES+="gnome-tweaks neofetch git net-tools htop timeshift deja-dup flatpak firefox chrome-gnome-shell screen nvidia-settings mangohud goverlay "
 if  [ "${OS_ID_LIKE}" = "arch" ]; then
-    PACKAGES+="sysstat python-pip veracrypt lutris protonup protonup-qt gamemode "
+    PACKAGES+="sysstat python-pip veracrypt lutris protonup protonup-qt gamemode baobab "
     # Setup libvirt for Single GPU Passhthrough - https://gitlab.com/risingprismtv/single-gpu-passthrough/-/wikis/4)-Configuring-of-Libvirt
     PACKAGES+="virt-manager qemu vde2 dnsmasq bridge-utils ovmf iptables-nft nftables ebtables "
 
@@ -139,6 +140,8 @@ else
     sudo apt install lutris
 
 fi
+
+flatpak install -y com.mattjakeman.ExtensionManager
 
 exit 0
 
